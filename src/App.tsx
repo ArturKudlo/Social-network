8c4759853ebcb966e6f87049ea5b220dda933018
arturkudli <arturkapes@gmail.com>
2021-07-27 21:59:41 +0300

    add header and tehnologies component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return <div className="App">social network</div>;
-}
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Technologies />
+    </div>
+  );
+};
 
+const Header = () => {
+  return (
+    <div>
+      <a href="#s">Home</a>
+      <a href="#s">News Feed</a>
+      <a href="#s">Messages</a>
+    </div>
+  );
+};
+
+const Technologies = () => {
+  return (
+    <div>
+      <ul>
+        <li>css</li>
+        <li>html</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
+    </div>
+  );
+};
 export default App;
