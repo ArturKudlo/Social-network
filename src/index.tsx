574ede5806e592af588fbee3b79b2114ea6d9e37
arturkudli <arturkapes@gmail.com>
2021-08-11 15:05:48 +0300

    creat local store and refactor state

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,31 @@
 import React from "react";
 import "./index.css";
-import state, { subscribe } from "./redux/state";
+import store from "./redux/state";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { addPost, RootStateType, updateNewPostText } from "./redux/state";
 
-let rerenderEntireTree = (state: RootStateType) => {
+let rerenderEntireTree = (state: any) => {
   ReactDOM.render(
     <React.StrictMode>
       <BrowserRouter>
         <App
           state={state}
-          addPost={addPost}
-          updateNewPostText={updateNewPostText}
+          addPost={store.addPost.bind(store)}
+          updateNewPostText={store.updateNewPostText.bind(store)}
         />
       </BrowserRouter>
     </React.StrictMode>,
     document.getElementById("root")
   );
 };
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
--- src/redux/state.ts
+++ src/redux/state.ts
@@ -1,7 +1,56 @@
-let rerenderEntireTree = (state: any) => {
-  console.log("state was changed");
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: "Hi, how are you?", likesCount: 20 },
+        { id: 2, message: "I's my first post", likesCount: 15 },
+      ],
+      newPostText: "it-kamasutra.com",
+    },
+    dialogsPage: {
+      dialogs: [
+        { id: 1, name: "Artur" },
+        { id: 2, name: "Maks" },
+        { id: 3, name: "Yuriy" },
+        { id: 4, name: "Anastasiya" },
+        { id: 5, name: "Dmitriy" },
+        { id: 6, name: "Vanda" },
+      ],
+      messages: [
+        { id: 1, message: "Hi" },
+        { id: 2, message: "How are you?" },
+        { id: 3, message: "Yo" },
+      ],
+    },
+    sidebar: {},
+  },
+  getState() {
+    return this._state;
+  },
+  _callSubscriber(state: any) {
+    console.log("state was changed");
+  },
+  addPost() {
+    let newPost: PostType = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0,
+    };
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newPostText = "";
+    //@ts-ignore
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newText: string) {
+    this._state.profilePage.newPostText = newText;
+    //@ts-ignore
+    this._callSubscriber(this._state);
+  },
+  subscribe(observer: any) {
+    //@ts-ignore
+    this._callSubscriber = observer;
+  },
 };
-
 
 type MessagType = {
   id: number;
@@ -37,50 +86,4 @@ export type RootStateType = {
   sidebar: SidebarType;
 };
 
-let state: RootStateType = {
-  profilePage: {
-    posts: [
-      { id: 1, message: "Hi, how are you?", likesCount: 20 },
-      { id: 2, message: "I's my first post", likesCount: 15 },
-    ],
-    newPostText: "it-kamasutra.com",
-  },
-  dialogsPage: {
-    dialogs: [
-      { id: 1, name: "Artur" },
-      { id: 2, name: "Maks" },
-      { id: 3, name: "Yuriy" },
-      { id: 4, name: "Anastasiya" },
-      { id: 5, name: "Dmitriy" },
-      { id: 6, name: "Vanda" },
-    ],
-    messages: [
-      { id: 1, message: "Hi" },
-      { id: 2, message: "How are you?" },
-      { id: 3, message: "Yo" },
-    ],
-  },
-  sidebar: {},
-};
-
-export const addPost = () => {
-  let newPost: PostType = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likesCount: 0,
-  };
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText = "";
-  rerenderEntireTree(state);
-};
-
-export const updateNewPostText = (newText: string) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-};
-
-export const subscribe = (observer: any) => {
-  rerenderEntireTree = observer;
-};
-
-export default state;
+export default store;
